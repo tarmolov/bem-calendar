b2241bd487701790ba09200f74145fab35e19b94
Alexander Tarmolov <tarmolov@gmail.com>
2013-09-01 02:37:47 +0400

    add model

---
+++ src/core/model/model.js
@@ -0,0 +1,25 @@
+modules.define('model', ['inherit', 'events', 'utils__compare'], function (provide, inherit, events, isEqual) {
+
+    provide(inherit(events.Emitter, {
+        __constructor: function (attributes) {
+            this._attributes = attributes || {};
+        },
+
+        set: function (name, value) {
+            var oldValue = this.get(name);
+
+            if (!isEqual(oldValue, value)) {
+                this._attributes[name] = value;
+                this.emit('change:' + name, {
+                    oldValue: oldValue,
+                    newValue: value
+                });
+            }
+        },
+
+        get: function (name) {
+            return this._attributes[name];
+        }
+    }));
+
+});
