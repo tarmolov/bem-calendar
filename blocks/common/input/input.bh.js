efd4207a251316f2d1d6f8a2fdca5fb4547cb2af
Alexander Tarmolov <tarmolov@gmail.com>
2013-09-09 19:16:03 +0400

    add tests for input

--- blocks/common/input/input.bh.js
+++ blocks/common/input/input.bh.js
@@ -2,6 +2,7 @@ module.exports = function (bh) {
 
     bh.match('input', function (ctx) {
         ctx.tag('input');
+        ctx.js(true);
         ctx.attr('placeholder', ctx.param('placeholder') || '');
         ctx.attr('value', ctx.content());
     });
